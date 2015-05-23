a9b0f926a3d624c83552d76b4f8583ebfbfb516f
Daniel Anderson <daniela@uoregon.edu>
2015-05-23 16:42:33 -0700

    Removed requirement for demo names list. It's now optional, with demos named generically if not provided.

--- R/batchPfile.r
+++ R/batchPfile.r
@@ -3,7 +3,7 @@
 #'
 #' This function looks at a directory, identifies all the pfiles in the directory based on a common string pattern, and returns a list with all person files (i.e., each element of the list represents a different pfile). 
 #'
-#' @param demNameL List containing the names of the demographic variables for pfiles.
+#' @param demNameL Optional list containing the names of the demographic variables for pfiles.
 #' @param dir Directory where pfiles are saved. Defaults to current working directory.
 #' @param r2WinstepsFile Logical. Was the r2Winsteps function called to produce the pfiles? Defaults to TRUE. Note that if FALSE all demographic variables will be read into a single variable.
 #'
@@ -11,19 +11,13 @@
 #'
 #' @return List of person files, with each element of the lit representing a sererate person file.
 
-batch.pfile<-function(demNameL, dir = getwd(), pattern = "Pfile", r2WinstepsFile = TRUE){ #THINK ABOUT DROPPING DATA FRAME REQUIREMENT
+batch.pfile<-function(demNameL = NULL, dir = getwd(), pattern = "Pfile", r2WinstepsFile = TRUE){ #THINK ABOUT DROPPING DATA FRAME REQUIREMENT
 	
 	oldDir<-getwd()
 	setwd(dir)
 
 	files<-list.files()
 	files<-files[grep(as.character(pattern),files)]
-
-	pFileNamesL<-vector("list",length(files))
-	for(i in 1:length(pFileNamesL)){
-		pFileNamesL[[i]]<-c("Dropped", "Entry", "Theta", "Status", "Count", "RawScore", "SE", "Infit", "Infit_Z", "Outfit", 
-	  		"Outfit_Z", "Displacement", "PointMeasureCorr", "Weight", "ObservMatch", "ExpectMatch", demNameL[[i]])	
-	}
 
 	widthL<-vector("list",length(files))
 	for(i in 1:length(files)){
@@ -51,16 +45,38 @@ batch.pfile<-function(demNameL, dir = getwd(), pattern = "Pfile", r2WinstepsFile
 	
 		for(i in 1:length(pfile)){
 			pfile[[i]]<-cbind(pfile[[i]][,-ncol(pfile[[i]])],demFiles[[i]])
-			names(pfile[[i]])<-pFileNamesL[[i]]
 		}
 	}
 	
+	pFileNamesL<-vector("list",length(files))
+
+		if(length(demNameL) > 0){
+			for(i in 1:length(pFileNamesL)){
+				pFileNamesL[[i]]<-c("Dropped", "Entry", "Theta", "Status", "Count", "RawScore", "SE", "Infit", "Infit_Z", "Outfit", 
+		  			"Outfit_Z", "Displacement", "PointMeasureCorr", "Weight", "ObservMatch", "ExpectMatch", demNameL[[i]])	
+			}
+		}
+		else{
+			for(i in 1:length(pFileNamesL)){
+				pFileNamesL[[i]]<-c("Dropped", "Entry", "Theta", "Status", "Count", "RawScore", "SE", "Infit", "Infit_Z", "Outfit", 
+		  			"Outfit_Z", "Displacement", "PointMeasureCorr", "Weight", "ObservMatch", "ExpectMatch", 
+		  			paste("v",17:ncol(pfile[[i]]), sep = ""))	
+			}
+
+		}
+	
+	if(r2WinstepsFile == TRUE){
+		for(i in 1:length(files)){
+			names(pfile[[i]])<-pFileNamesL[[i]]	
+		}
+	}
+
 	if(r2WinstepsFile == FALSE){
 		for(i in 1:length(pfile)){
 			names(pfile[[i]])<-c(pFileNamesL[[i]][1:(ncol(l[[1]]) -1) ],"NAME")
 		}
 	}
-	
+
 	 pfile<-lapply(pfile,function(x){
 		x<-x[,-1]
 	return(x)
