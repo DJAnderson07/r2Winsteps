385084e8974380dd4839ffcbcd973c5be47786f5
Daniel Anderson <daniela@uoregon.edu>
2015-05-24 20:17:44 -0700

    Actually added partial credit capabilities (last version did not push correctly)

--- R/r2Winsteps.r
+++ R/r2Winsteps.r
@@ -4,184 +4,261 @@
 #'
 #' @param itms Dataframe or matrix of items responses
 #' @param dems Dataframe or matrix of person identifiers/demographic fields
-#' @param title Title of the analysis, which will be used in the name of the data and text files. Defaults to "r2Winsteps".
+#' @param title Title of the analysis, which will be used in the name of the 
+#'   data and text files. Defaults to "r2Winsteps".
 #' @param ifile Logical, default to TRUE. Should item files be returned?
 #' @param pfile Logical, default to TRUE. Should person files be returned?
-#' @param format Format in which item and person files should be returned. Takes values "txt" and "XLS" to return txt and XLS files, 
-#' respectively. Defaults to "txt". Note that "txt" must be used if subsequent calls to batch.ifile/batch.pfile are used to read the
-#' files back into R.
+#' @param format Format in which item and person files should be returned. 
+#'   Takes values "txt" and "XLS" to return txt and XLS files, respectively. 
+#'   Defaults to "txt". Note that "txt" must be used if subsequent calls to 
+#'   batch.ifile/batch.pfile are used to read thefiles back into R.
 #' @export
 #' @return Control and Data file for analysis with Winsteps.
 
 ##### Build in capability for partial credit model
 
-r2Winsteps<-function(itms, dems, title = "r2Winsteps", Ifile = TRUE, Pfile = TRUE, format = "txt"){
+r2Winsteps<-function (itms, dems, partialCredit = FALSE, title = "r2Winsteps", 
+    Ifile = TRUE, Pfile = TRUE, format = "txt"){
 
-	##### Write Data
-	itms<-apply(itms,2,as.character)
-	for(i in 1:ncol(itms)){
-		itms[,i]<-ifelse(is.na(itms[,i]),"M",itms[,i])
-	}
-	maxWide<-max(apply(itms,2,function(x) nchar(x)))
-
-	for(i in 1:ncol(itms)){
-		itms[,i]<-stringr::str_pad(itms[,i],maxWide, side = "right")
-	}
-
-	dtaCodes<-apply(itms,2,table)
-	codes<-c(names(dtaCodes[[1]]),names(dtaCodes[[2]]))
-	for(i in 3:length(dtaCodes)){
-		codes<-c(codes,names(dtaCodes[[i]]))
-	}
-	codes<-unique(codes)
-
-	istring<-paste(itms[,1],itms[,2],sep = "")
-	for(i in 3:ncol(itms)){
-		istring<-paste(istring,itms[,i],sep = "")
-	}
-
-	for(i in 1:ncol(dems)){
-		dems[,i]<-as.character(dems[,i])
-		dems[,i]<-stringr::str_pad(dems[,i], max(nchar(dems[,i])),side = "right")
-	}
-
-	dstring<-paste(dems[,1],dems[,2], sep = " | ")
-	for(i in 3:ncol(dems)){
-		dstring<-paste(dstring,dems[,i], sep = " | ")
-	}
-
-	d<-data.frame(dems = dstring,responses = istring)
-	d$dems<-as.character(d$dems)
-
-	winData<-paste(d$dems,d$responses,sep = " ")
-	
-	dtaTitle<-paste(as.character(title),"Dta",sep = "")
-	sink(paste(dtaTitle,"txt",sep = "."))
-	cat(winData,sep = "\n")
-	sink()
-
-	
-
-	###### Write Control file
-	ttl<-paste("TITLE = ",title,sep = "")
-		dtaTitle<-paste(as.character(title),"Dta",sep = "")
-	dt<-paste("DATA = ",paste(dtaTitle,"txt",sep = "."))
-	i1<-paste("ITEM1 = ",nchar(dstring)[1] + 2)
-	ni<-paste("NI = ",ncol(itms))
-	namStart<-"NAME1 = 1"
-	namLen<-paste("NAMLEN = ",nchar(dstring)[1])
-	wid<-paste("XWIDE = ", maxWide, sep = "")
-	cod<-paste("CODES = ", paste(codes, collapse = " "), sep = "")
-	totSc<-"TOTALSCORE = YES"
-	
-	if(format == "XLS"){
-		ifile<-paste("IFILE = ",paste(title,"Ifile.xls",sep = ""))
-		pfile<-paste("PFILE = ",paste(title,"Pfile.xls",sep = ""))
-	}
-	
-	if(format == "txt"){
-		ifile<-paste("IFILE = ",paste(title,"Ifile.txt",sep = ""))
-		pfile<-paste("PFILE = ",paste(title,"Pfile.txt",sep = ""))
-	}
-
-	#CREATE PERSON LABELS
-	dLen<-rep(NA,ncol(dems))
-	for(i in 1:length(dLen)){
-		dLen[i]<-max(nchar(dems[,i]))
-	}
-
-	dLengths<-data.frame(names(dems),dLen,first = cumsum(dLen) - dLen + 2,last = cumsum(dLen) + 1)
-		dLengths[1,3]<-dLengths[1,3] - 1
-	demoLengths<-paste(dLengths$first,dLengths$last,sep = "E")
-
-	finalDemScript<-paste(
-		paste("@",names(dems),sep = ""),
-		demoLengths,sep = " = ")
-
-	#Combine all elements into final control file
-	cntrlTitle<-paste(as.character(title),"Cntrl",sep = "")
-	
-	if(Ifile == TRUE & Pfile == TRUE){
-		sink(paste(cntrlTitle,"txt",sep = "."))
-			cat(
-				cat("&INST",
-					ttl,
-					dt,
-					i1,
-					ni,  
-					namStart,
-					namLen,
-					wid,
-					cod,
-					totSc,
-					ifile,
-					pfile,
-				sep = "\n"),
-				cat(finalDemScript,"&End",sep = "\n"),
-				cat(colnames(itms),"END NAMES",sep = "\n")
-			)
-		sink()	
-	}
-
-	if(Ifile == TRUE & Pfile == FALSE){
-		sink(paste(cntrlTitle,"txt",sep = "."))
-			cat(
-				cat("&INST",
-					ttl,
-					dt,
-					i1,
-					ni,  
-					namStart,
-					namLen,
-					wid,
-					cod,
-					totSc,
-					ifile,
-				sep = "\n"),
-				cat(finalDemScript,"&End",sep = "\n"),
-				cat(colnames(itms),"END NAMES",sep = "\n")
-			)
-		sink()	
-	}
-
-	if(Ifile == FALSE & Pfile == TRUE){
-		sink(paste(cntrlTitle,"txt",sep = "."))
-			cat(
-				cat("&INST",
-					ttl,
-					dt,
-					i1,
-					ni,  
-					namStart,
-					namLen,
-					wid,
-					cod,
-					totSc,
-					pfile,
-				sep = "\n"),
-				cat(finalDemScript,"&End",sep = "\n"),
-				cat(colnames(itms),"END NAMES",sep = "\n")
-			)
-		sink()	
-	}
-
-	if(Ifile == FALSE & Pfile == FALSE){
-		sink(paste(cntrlTitle,"txt",sep = "."))
-			cat(
-				cat("&INST",
-					ttl,
-					dt,
-					i1,
-					ni,  
-					namStart,
-					namLen,
-					wid,
-					cod,
-					totSc,
-				sep = "\n"),
-				cat(finalDemScript,"&End",sep = "\n"),
-				cat(colnames(itms),"END NAMES",sep = "\n")
-			)
-		sink()	
-	}
-}	
+#=============================== Write Data file ===============================
+	#Convert itms to character, code missing data as "M"
+    itms <- apply(itms, 2, as.character)
+    for (i in 1:ncol(itms)) {
+        itms[, i] <- ifelse(is.na(itms[, i]), "M", itms[, i])
+    }
+    
+    #Find the column with the maximium width
+    maxWide <- max(apply(itms, 2, function(x) nchar(x)))
+    
+    #Pad all items to equal the max width
+    for (i in 1:ncol(itms)) {
+        itms[, i] <- stringr::str_pad(itms[, i], maxWide, side = "right")
+    }
+    
+    #Extract codes in data
+    dtaCodes <- apply(itms, 2, table)
+    if (class(dtaCodes) == "matrix") {
+        codes <- rownames(dtaCodes)
+    }
+    else {
+        codes <- c(names(dtaCodes[[1]]), names(dtaCodes[[2]]))
+        for (i in 3:length(dtaCodes)) {
+            codes <- c(codes, names(dtaCodes[[i]]))
+        }
+        codes <- unique(codes)
+    }
+    codes <- codes[-grep("M", codes)]
+    
+    #Paste all items in one character string
+    istring <- paste(itms[, 1], itms[, 2], sep = "")
+    for (i in 3:ncol(itms)) {
+        istring <- paste(istring, itms[, i], sep = "")
+    }
+    
+    #Convert all dems columns to string; pad to largest column
+    for (i in 1:ncol(dems)) {
+        dems[, i] <- as.character(dems[, i])
+        dems[, i] <- stringr::str_pad(dems[, i], max(nchar(dems[, 
+            i])), side = "right")
+    }
+    
+    #Paste demos into a single string
+    dstring <- paste(dems[, 1], dems[, 2], sep = " | ")
+    for (i in 3:ncol(dems)) {
+        dstring <- paste(dstring, dems[, i], sep = " | ")
+    }
+    
+    #Put items and demos together
+    d <- data.frame(dems = dstring, responses = istring)
+    
+    #Demo string (not a factor)
+    d$dems <- as.character(d$dems)
+    
+    #Put data together
+    winData <- paste(d$dems, d$responses, sep = " ")
+    
+    #Title for Winsteps data file
+    dtaTitle <- paste(as.character(title), "Dta", sep = "")
+    sink(paste(dtaTitle, "txt", sep = "."))
+    	cat(winData, sep = "\n")
+    sink()
+    
+#============================= Write Control File ==============================
+    ttl <- paste("TITLE = ", title, sep = "")
+    dtaTitle <- paste(as.character(title), "Dta", sep = "")
+    dt <- paste("DATA = ", paste(dtaTitle, "txt", sep = "."))
+    i1 <- paste("ITEM1 = ", nchar(dstring)[1] + 2)
+    ni <- paste("NI = ", ncol(itms))
+    namStart <- "NAME1 = 1"
+    namLen <- paste("NAMLEN = ", nchar(dstring)[1])
+    wid <- paste("XWIDE = ", maxWide, sep = "")
+    cod <- paste("CODES = ", paste(codes, collapse = " "), sep = "")
+    totSc <- "TOTALSCORE = YES"
+    
+    if (format == "XLS") {
+        ifile <- paste("IFILE = ", paste(title, "Ifile.xls", 
+            sep = ""))
+        pfile <- paste("PFILE = ", paste(title, "Pfile.xls", 
+            sep = ""))
+    }
+    
+    if (format == "txt") {
+        ifile <- paste("IFILE = ", paste(title, "Ifile.txt", 
+            sep = ""))
+        pfile <- paste("PFILE = ", paste(title, "Pfile.txt", 
+            sep = ""))
+    }
+    
+    #Calculate peson variable lenghts
+    dLen <- rep(NA, ncol(dems))
+    for (i in 1:length(dLen)) {
+        dLen[i] <- max(nchar(dems[, i]))
+    }
+    dLengths <- data.frame(names(dems), dLen, first = cumsum(dLen) - 
+        dLen + 2, last = cumsum(dLen) + 1)
+    dLengths[1, 3] <- dLengths[1, 3] - 1
+    demoLengths <- paste(dLengths$first, dLengths$last, sep = "E")
+    finalDemScript <- paste(paste("@", names(dems), sep = ""), 
+        demoLengths, sep = " = ")
+    
+    #Control File Name
+    cntrlTitle <- paste(as.character(title), "Cntrl", sep = "")
+    
+#================================= Write Files =================================
+    if (Ifile == TRUE & Pfile == TRUE & length(codes) == 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, ifile, pfile, sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (Ifile == TRUE & Pfile == FALSE & length(codes) == 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, ifile, sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (Ifile == FALSE & Pfile == TRUE & length(codes) == 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, pfile, sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (Ifile == FALSE & Pfile == FALSE & length(codes) == 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (partialCredit == TRUE & length(codes) == 2) {
+        warning(paste("Control file not written. Only two item categories", 
+        	"were detected, but partialCredit was specified as TRUE"))
+    }
+    if (Ifile == TRUE & Pfile == TRUE & length(codes) > 2 & 
+    	partialCredit == TRUE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, ifile, pfile, "GROUPS = 0", sep = "\n"), 
+            	cat(finalDemScript, "&End", sep = "\n"), 
+            	cat(colnames(itms), "END NAMES", sep = "\n")
+            	)
+        	sink()
+    }
+    if (Ifile == TRUE & Pfile == FALSE & length(codes) > 2 & 
+        partialCredit == TRUE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod,
+        				totSc, ifile, "GROUPS = 0", sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (Ifile == FALSE & Pfile == TRUE & length(codes) > 2 & 
+        partialCredit == TRUE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, pfile, "GROUPS = 0", sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (Ifile == FALSE & Pfile == FALSE & length(codes) > 2 & 
+        partialCredit == TRUE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, "GROUPS = 0", sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+    if (Ifile == TRUE & Pfile == TRUE & length(codes) > 2 & 
+    	partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, ifile, pfile, "GROUPS = ' ' ", sep = "\n"), 
+            	cat(finalDemScript, "&End", sep = "\n"), 
+            	cat(colnames(itms), "END NAMES", sep = "\n")
+            	)
+        	sink()
+    }
+    if (Ifile == TRUE & Pfile == FALSE & length(codes) > 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, ifile, "GROUPS = ' ' ", sep = "\n"), 
+            		cat(finalDemScript, "&End", sep = "\n"), 
+            		cat(colnames(itms), "END NAMES", sep = "\n")
+            		)
+        	sink()
+    }
+    if (Ifile == FALSE & Pfile == TRUE & length(codes) > 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod, 
+        				totSc, pfile, "GROUPS = ' ' ", sep = "\n"), 
+            		cat(finalDemScript, "&End", sep = "\n"), 
+            		cat(colnames(itms), "END NAMES", sep = "\n")
+            		)
+        	sink()
+    }
+    if (Ifile == FALSE & Pfile == FALSE & length(codes) > 2 & 
+        partialCredit == FALSE) {
+        	sink(paste(cntrlTitle, "txt", sep = "."))
+        		cat(
+        			cat("&INST", ttl, dt, i1, ni, namStart, namLen, wid, cod,
+        				totSc, "GROUPS = ' ' ", sep = "\n"), 
+        			cat(finalDemScript, "&End", sep = "\n"), 
+        			cat(colnames(itms), "END NAMES", sep = "\n")
+        			)
+        	sink()
+    }
+}
