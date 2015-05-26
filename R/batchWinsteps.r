952ea6ae4b33dafb50ab15a89f28b7bc2510824e
Daniel Anderson <daniela@uoregon.edu>
2015-05-26 15:05:03 -0700

    Modified code to align with 80 character wide rule

--- R/batchWinsteps.r
+++ R/batchWinsteps.r
@@ -1,12 +1,19 @@
 #' Batch Process Winsteps Files
 #' 
-#' This function looks at a directory and creates a Winsteps batch processing file based on all the control files in the directory.
+#' This function looks at a directory and creates a Winsteps batch processing 
+#'   file based on all the control files in the directory.
 #' @param batchName The name of the batch file to be saved.
-#' @param dir Directory where multiple control files are saved. Defaults to current directory.
-#' @param outFileNames Character vector of output file names from Winsteps analyses. Defaults to NULL, and the function creates names based on the names of the control files.
-#' @param pattern Common character pattern represented in control files in the directory. Defaults to "Cntrl", which is what is automatically generated when r2Winsteps is called.
+#' @param dir Directory where multiple control files are saved. Defaults to 
+#'   current directory.
+#' @param outFileNames Character vector of output file names from Winsteps 
+#'   analyses. Defaults to NULL, and the function creates names based on the 
+#'   names of the control files.
+#' @param pattern Common character pattern represented in control files in the 
+#'   directory. Defaults to "Cntrl", which is what is automatically generated 
+#'   when r2Winsteps is called.
 
-batchWinsteps<-function(batchName, dir = getwd(), outFileNames = NULL, pattern = "Cntrl"){
+batchWinsteps<-function(batchName, dir = getwd(), outFileNames = NULL, 
+	pattern = "Cntrl"){
 	oldD<-getwd()
 	setwd(dir)
 
@@ -14,11 +21,13 @@ batchWinsteps<-function(batchName, dir = getwd(), outFileNames = NULL, pattern =
 	files<-files[grep(as.character(pattern),files)]
 
 	if(length(outFileNames) == 0 & pattern == "Cntrl"){
-		outFileNames<-paste(substr(files,1,nchar(files) - 9),"Out",".txt",sep = "")
+		outFileNames<-paste(substr(files,1,nchar(files) - 9),"Out",".txt",
+			sep = "")
 	}
 
 	if(length(outFileNames) == 0 & pattern != "Cntrl"){
-		outFileNames<-paste(substr(files,1,nchar(files) - 4),"Out",".txt",sep = "")
+		outFileNames<-paste(substr(files,1,nchar(files) - 4),"Out",".txt",
+			sep = "")
 	}
 	
 	first<-rep("START /w C:\\winsteps\\WINSTEPS BATCH=YES ",length(files))
